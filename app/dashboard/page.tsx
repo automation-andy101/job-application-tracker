93acb26d2eae173a4f5e34df41993de06ae3529d
Andy <automation.andy101@gmail.com>
2026-02-21 16:50:40 +0000

    Started adding dashboard component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,37 @@
-export default function Dashboard() {
-    return <div>Dashboard Page</div>
+import { getSession } from "@/lib/auth/auth";
+import connectDB from "@/lib/db";
+import { Board } from "@/lib/models";
+import { redirect } from "next/navigation";
+
+
+export default async function Dashboard() {
+    const session = await getSession();
+
+    if (!session?.user) {
+        redirect("/sign-up");
+    }
+
+    await connectDB();
+
+    const board = await Board.findOne({
+        userId: session.user.id,
+        name: "Job Hunt"
+    });
+
+    console.log(board);
+    
+    return (
+        <div className="min-h-screen bg-white">
+            <div className="container mx-auto p6">
+                <div className="mb-6">
+                    <h1 className="text-3xl font-bold text-black">{board.name}</h1>
+                    <p className="text-gray-600">
+                        Track your job applications
+                    </p>
+
+
+                </div>
+            </div>
+        </div>
+    )
 }
